e95dc45d25a45e49abe21443b6ca512a2f2392f5
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-18 11:07:15 +0700

    Add files via upload

---
+++ tugas4.js
@@ -0,0 +1,17 @@
+var tinggi_siswa1 = 70;
+var tinggi_siswa2 = 67;
+var tinggi_siswa3 = 71;
+
+if (tinggi_siswa1 > tinggi_siswa2){
+  if (tinggi_siswa1 > tinggi_siswa3){
+     console.log("Siswa yang paling tinggi");
+  }else{
+     console.log("Siswa tertinggi kedua");
+  }
+}else{
+  if (tinggi_siswa1 < tinggi_siswa3){
+     console.log("yang terpendek");
+  }else{
+     console.log("yang terpendek kedua");
+  }
+}
